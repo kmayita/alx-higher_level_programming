bbaa932370a11ec71a3cdf1851cde1d88fe91e15
kmayita <umamaya7766@gmail.com>
2023-01-08 13:55:52 +0000

    addpy

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif argv_count is 1:
+        print("{:d} argument:".format(argv_count))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argv_count))
+        while index <= argv_count:
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+            index += 1
